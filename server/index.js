82fbe689c3f0698bc33608a0f2fffff841d5b282
Advaith Narayanan <112336645+AdvaithN1@users.noreply.github.com>
2024-03-20 23:25:07 +0000

    fixed pointer

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-});
+
+const publicPath = path.join(__dirname, '..', 'client');
+app.use(express.static(publicPath, { index: 'index.html' }));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
